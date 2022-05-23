29c68a3f6cc55abb51daf63b0ce43c6bf02ddce4
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-05-23 12:41:38 +0200

    Setup basic Spring-Project

---
+++ src/main/java/ALP/KBECurrency/KbeCurrencyApplication.java
@@ -0,0 +1,13 @@
+package ALP.KBECurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeCurrencyApplication.class, args);
+	}
+
+}
